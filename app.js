9f71a7d942f04fafe2204c75d81e4cde7a91456a
Spyros Argalias <sargalias@gmail.com>
2018-04-07 21:41:48 +0100

    url creation functionality working, but bundled into app.js file

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const validUrl = require('valid-url');
 const mongoose = require('mongoose');
 const DATABASE_URI = require('./config/database').DATABASE_URI;
+const autoIncrement = require('mongoose-auto-increment');
 
 // Run Express
 const app = express();
@@ -16,6 +17,22 @@ mongoose.connect(DATABASE_URI);
 const db = mongoose.connection;
 mongoose.Promise = global.Promise;
 db.on('error', console.error.bind(console, 'connection error:'));
+autoIncrement.initialize(db);
+
+
+// Model
+const urlSchema = new mongoose.Schema({
+    originalUrl: {type: String, required: true},
+});
+
+urlSchema.plugin(autoIncrement.plugin, {
+    model: 'URL',
+    field: 'shortened',
+    startAt: 101
+});
+
+const UrlModel = mongoose.model('URL', urlSchema);
+
 
 
 app.get('/', (req, res) => {
@@ -24,8 +41,14 @@ app.get('/', (req, res) => {
 
 app.get('/api/new/:url(*)', (req, res) => {
     if (validUrl.isUri(req.params.url)) {
-        console.log('valid uri');
-        res.send('API home');
+        UrlModel.create({
+            originalUrl: req.params.url
+        }, (err, data) => {
+            if (err) {
+                return res.send(err);
+            }
+            return res.send(data);
+        });
     } else {
         res.json({error: "Wrong URL format. Please make sure you have a valid protocol and real site."});
     }
