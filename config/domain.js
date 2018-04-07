9c9de2933af758d557a9588fea2cb727eba3dacc
Spyros Argalias <sargalias@gmail.com>
2018-04-08 00:11:37 +0100

    fix config domain for production environment

--- config/domain.js
+++ config/domain.js
@@ -1,4 +1,4 @@
-const DOMAIN = process.env.NODE_ENV === 'prod' ? 'https://sargalias-url-shortener-ms.herokuapp.com/' : 'http://localhost:8080/';
+const DOMAIN = process.env.NODE_ENV === 'production' ? 'https://sargalias-url-shortener-ms.herokuapp.com/' : 'http://localhost:8080/';
 const APIHOME = 'api/';
 
 module.exports = {DOMAIN: DOMAIN, APIHOME: APIHOME};
